bf77952595f05db2adc0b2d79628fe1f5c5133ed
Zalán Bálint Lévai <zalan.levai@gmail.com>
2019-11-07 01:15:35 +0000

    Add the Bios firmware type
    Add the Bios type to manage the system initialization process

--- src/chip8/chip8.rs
+++ src/chip8/chip8.rs
@@ -1,14 +1,20 @@
-use crate::chip8::{Processor, Memory};
+use crate::chip8::{Processor, Memory, Bios};
 
 pub struct Chip8 {
     pub processor: Processor,
-    pub memory: Memory
+    pub memory: Memory,
+    pub bios: Bios
 }
 impl Chip8 {
     pub fn new() -> Self {
         Self {
             processor: Processor::new(),
-            memory: Memory::new()
+            memory: Memory::new(),
+            bios: Bios::new()
         }
+    }
+
+    pub fn init(&mut self) {
+        self.bios.load(&mut self.memory);
     }
 }
--- src/chip8/mod.rs
+++ src/chip8/mod.rs
@@ -2,8 +2,10 @@ pub mod chip8;
 pub mod processor;
 pub mod opcode;
 pub mod memory;
+pub mod bios;
 
 pub use self::chip8::Chip8;
 pub use self::processor::{Processor, Registers};
 pub use self::opcode::Opcode;
 pub use self::memory::Memory;
+pub use self::bios::Bios;
